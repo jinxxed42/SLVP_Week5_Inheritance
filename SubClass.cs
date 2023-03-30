15798be85948dbeff21b840a07666c3abc1a0a7a
Claus Damgaard <jinxxed@outlook.com>
2023-03-30 17:07:34 +0200

    Removed unnecessary using directives.

--- SubClass.cs
+++ SubClass.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SLVP_Week5_Inheritance
+﻿namespace SLVP_Week5_Inheritance
 {
     internal class SubClass : BaseClass
     {
